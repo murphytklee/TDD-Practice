e0f794fad2cbb37644d71f62adf1c60b7536e21c
murphytklee <murphytklee@gmail.com>
2024-10-08 17:08:31 +0900

    test : service 멤버십등록 테스트 추가

---
+++ src/main/java/com/example/demo/exception/MembershipErrorResult.java
@@ -0,0 +1,20 @@
+package com.example.demo.exception;
+
+import org.springframework.http.HttpStatus;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum MembershipErrorResult {
+
+    NOT_MEMBERSHIP_OWNER(HttpStatus.BAD_REQUEST, "Not a membership owner"),
+    MEMBERSHIP_NOT_FOUND(HttpStatus.NOT_FOUND, "Membership Not found"),
+    DUPLICATED_MEMBERSHIP_REGISTER(HttpStatus.BAD_REQUEST, "Duplicated Membership Register Request"),
+    UNKNOWN_EXCEPTION(HttpStatus.INTERNAL_SERVER_ERROR, "Unknown Exception"),
+    ;
+
+    private final HttpStatus httpStatus;
+    private final String message;
+}
