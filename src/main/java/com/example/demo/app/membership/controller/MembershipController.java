e663ea36bd1030718e68b24412c087394bd6c1b2
murphytklee <murphytklee@gmail.com>
2024-10-22 16:51:10 +0900

    test : 멤버십 상세 조회 API Controller 테스트 추가

--- src/main/java/com/example/demo/app/membership/controller/MembershipController.java
+++ src/main/java/com/example/demo/app/membership/controller/MembershipController.java
@@ -9,6 +9,7 @@ import javax.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -43,4 +44,8 @@ public class MembershipController {
         return ResponseEntity.ok(membershipService.getMembershipList(userId));
     }
     
+    @GetMapping("/api/v1/memberships/{id}")
+    public ResponseEntity<MembershipDetailResponse> getMembership(@RequestHeader(USER_ID_HEADER) final String userId, @PathVariable final Long id) {
+        return ResponseEntity.ok(membershipService.getMembership(id, userId));
+    }
 }
