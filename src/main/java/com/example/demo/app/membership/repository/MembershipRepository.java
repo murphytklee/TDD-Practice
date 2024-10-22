a3c4e6e8e6bca5e7b841dba2d4b994b823c5c0bf
murphytklee <murphytklee@gmail.com>
2024-10-22 15:05:19 +0900

    test : 멤버십 전체 조회 API Repository 테스트 추가

--- src/main/java/com/example/demo/app/membership/repository/MembershipRepository.java
+++ src/main/java/com/example/demo/app/membership/repository/MembershipRepository.java
@@ -1,5 +1,7 @@
 package com.example.demo.app.membership.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +12,6 @@ import com.example.demo.app.membership.entity.Membership;
 public interface MembershipRepository extends JpaRepository<Membership, Long> {
 
     Membership findByUserIdAndMembershipType(final String userId, final MembershipType membershipType);
+
+    List<Membership> findAllByUserId(final String userId);
 }
