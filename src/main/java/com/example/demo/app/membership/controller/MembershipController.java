35b47f060542ae0b92de5aeb8c9e8c7eee55bf02
Murphy <111736036+murphytklee@users.noreply.github.com>
2024-10-22 13:23:09 +0900

    refactor : 멤버십 등록 AOP 추가 및 Controller 리팩토링 (#3)

--- src/main/java/com/example/demo/app/membership/controller/MembershipController.java
+++ src/main/java/com/example/demo/app/membership/controller/MembershipController.java
@@ -1,5 +1,9 @@
 package com.example.demo.app.membership.controller;
 
+import static com.example.demo.app.membership.constants.MembershipConstants.USER_ID_HEADER;
+
+import javax.validation.Valid;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,16 +13,24 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.app.membership.dto.MembershipRequest;
 import com.example.demo.app.membership.dto.MembershipResponse;
+import com.example.demo.app.membership.service.MembershipService;
 
-import static com.example.demo.app.membership.constants.MembershipConstants.USER_ID_HEADER;
+import lombok.RequiredArgsConstructor;
 
 @RestController
+@RequiredArgsConstructor
 public class MembershipController {
+
+    private final MembershipService membershipService;
     
     @PostMapping("/api/v1/memberships")
-    public ResponseEntity<MembershipResponse> addMembership(@RequestHeader(USER_ID_HEADER) final String userId, @RequestBody final MembershipRequest membershipRequest) {
+    public ResponseEntity<MembershipResponse> addMembership(@RequestHeader(USER_ID_HEADER) final String userId, @RequestBody @Valid final MembershipRequest membershipRequest) {
+        membershipService.addMembership(userId, membershipRequest.getMembershipType(), membershipRequest.getPoint());
         
-        return ResponseEntity.status(HttpStatus.CREATED).build();
+        final MembershipResponse membershipResponse = membershipService.addMembership(userId, membershipRequest.getMembershipType(), membershipRequest.getPoint());
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                             .body(membershipResponse);
     }
     
 }
